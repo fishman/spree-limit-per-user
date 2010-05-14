a3b4450484354ec45cfe01bb2340e312b46e7977
Reza Jelveh <toyb0mb@gmx.net>
2010-05-14 13:15:53 +0200

    temporarily enforce a one item per user limit

--- limit_per_user_hooks.rb
+++ limit_per_user_hooks.rb
@@ -1,5 +1,7 @@
 class LimitPerUserHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_product_form_right, 'shared/limit_per_user'
+  replace :inside_product_cart_form, 'shared/cart_form'
+  replace :cart_item_quantity, 'shared/order_quantity'
 
 end
